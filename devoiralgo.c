2733ecd7943a5d33a226d115d7ca99716e14e9fb
aminata1709 <rassulaminatademe@gmail.com>
2025-06-17 20:19:05 +0000

    Update devoiralgo.c

--- devoiralgo.c
+++ devoiralgo.c
@@ -79,8 +79,65 @@ int Maxtab(struct facture tabf[N]){
 
 
 //Fonction somme par objetAmina
+Fonction sommeParObjet(F: tableau de Facture, N: entier, obj: chaîne): réel
+    total ← 0
+    Pour i de 1 à N faire
+        Si F[i].objet = obj Alors
+            total ← total + F[i].montant
+        FinSi
+    FinPour
+    Retourner total
+FinFonction
+
 
 //procédure facture par étudiantAmina
+Procédure factureParEtudiant(F: tableau de Facture, N: entier, id: entier)
+    Pour i de 1 à N faire
+        Si F[i].id_etudiant = id Alors
+            Écrire("Numéro: ", F[i].numero)
+            Écrire("Montant: ", F[i].montant)
+            Écrire("Objet: ", F[i].objet)
+            Écrire("Inscription: ", F[i].inscription)
+            Écrire("Scolarité: ", F[i].scolarité)
+        FinSi
+    FinPour
+FinProcédure
 
 //mainAmina
+Début
+    Déclarer F[100] comme tableau de Facture
+    Déclarer N, choix, id: entier
+    Déclarer obj: chaîne
+    Écrire("Nombre de factures : ") Lire(N)
+    Appeler remplirTab(F, N)
+
+    Répéter
+        Écrire("Menu :")
+        Écrire("1. Afficher les factures")
+        Écrire("2. Somme totale des factures")
+        Écrire("3. Montant minimum")
+        Écrire("4. Montant maximum")
+        Écrire("5. Somme par objet")
+        Écrire("6. Factures par étudiant")
+        Écrire("0. Quitter")
+        Lire(choix)
+
+        Selon choix faire
+            Cas 1:
+                Appeler afficheTab(F, N)
+            Cas 2:
+                Écrire("Somme: ", sommeTab(F, N))
+            Cas 3:
+                Écrire("Minimum: ", minTab(F, N))
+            Cas 4:
+                Écrire("Maximum: ", maxTab(F, N))
+            Cas 5:
+                Écrire("Objet à rechercher: ") Lire(obj)
+                Écrire("Somme pour ", obj, " : ", sommeParObjet(F, N, obj))
+            Cas 6:
+                Écrire("ID étudiant: ") Lire(id)
+                Appeler factureParEtudiant(F, N, id)
+        FinSelon
+    Jusqu’à choix = 0
+Fin
 
