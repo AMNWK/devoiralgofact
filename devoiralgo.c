5c3f9c6459c2b7cb840a74aeec5527de52d7f05b
AMNWK <charonneuh@gmail.com>
2025-06-17 11:13:05 +0000

    Create devoiralgo.c

---
+++ devoiralgo.c
@@ -0,0 +1,33 @@
+//remplirtab Aida
+const int N=10;
+void remplir tabp(struct facture tabf[N]){
+	printf("saisir lenumero du facture");
+	scanf("%d",&tabf[i].numero);
+	printf("saisir le montant");
+	scanf("%d",&tabf[i].montant);
+	printf("saisir l'id de l'étudiant");
+	scanf("%d",&tabf[i].id);
+}
+	
+
+
+
+//affichertabAida
+
+
+
+//sommetab Aida
+
+// Définir la structure Ange
+
+//minTabAnge
+
+// maxtabange
+
+
+//Fonction somme par objetAmina
+
+//procédure facture par étudiantAmina
+
+//mainAmina
+
