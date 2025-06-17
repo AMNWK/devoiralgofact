1f6c820ea1246c2227a16639b585e41344917466
AMNWK <charonneuh@gmail.com>
2025-06-17 13:21:13 +0000

    Update devoiralgo.c

--- devoiralgo.c
+++ devoiralgo.c
@@ -1,22 +1,36 @@
 //remplirtab Aida
 const int N=10;
-void remplir tabp(struct facture tabf[N]){
+void remplirtabp(struct facture tabf[N]){
 	printf("saisir lenumero du facture");
 	scanf("%d",&tabf[i].numero);
 	printf("saisir le montant");
 	scanf("%d",&tabf[i].montant);
+    printf("saisir l'objet de la facture");
+    scanf("%d", tabf[i].objet);
 	printf("saisir l'id de l'étudiant");
 	scanf("%d",&tabf[i].id);
 }
-	
-
-
 
 //affichertabAida
+void afficherTab(struct facture tabf[N]) {
+    printf("\nListe des factures :\n");
+    for( i = 0; i < n; i++) {
+        printf("Facture %d : Numéro=%d, Montant=%d, Objet=%d, ID étudiant=%d\n",
+             tab[i].numero, tab[i].montant, tab[i].objet, tab[i].id_etudiant);
+    }
+}
 
 
 
 //sommetab Aida
+int sommeTab(struct facture tabf[N]) {
+    int somme = 0;
+    for(int i = 0; i < N; i++) {
+        somme = somme +tabf[i].montant;
+    }
+    return somme;
+}
+
 
 // Définir la structure Ange
 
